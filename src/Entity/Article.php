885f0c8f4d59c5be8d1a0585325b0351ca66397e
devJeremie <chabanaisjeremie@gmail.com>
2024-02-17 18:09:22 +0100

    ApiWebNewspaper

---
+++ src/Entity/Article.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $coverText;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCoverText(): ?string
+    {
+        return $this->coverText;
+    }
+
+    public function setCoverText(string $coverText): self
+    {
+        $this->coverText = $coverText;
+
+        return $this;
+    }
+}
